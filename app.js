a52f48aeeaf635b1202eaf1b4f83687c3dd236f6
Jearranai Jujaroen <jear.jujaroen@gmail.com>
2015-05-22 16:32:41 -0400

    Separated keyup on #submit-box2 and #add-category, fixed bug on both, change newli() to newLi() and newDt(), fixed bug on empty and changed to emptyShoppingList() and emptyInventory()

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ $(document).ready(function () {
     $( "form" ).submit(function( event ) {
     event.preventDefault();
     })
-    var newli = function(n) {
-        $('#items' + n + ' ol').append('<li><span></span></li>');
+    var newLi = function() {
+        $('#items1 ol').append('<li><span></span></li>');
     };
-    newli(1);
-    newli(2);
+    var newDt = function() {
+        $('#items2 dl').append('<dt><span></span></dt>');
+    };
+    newLi();
+    newDt();
+    
     var newXbutton = function() {
         var x = $('<i class="fa fa-times"></i>');
         x.click(function() {          
@@ -15,6 +19,7 @@ $(document).ready(function () {
         })
         return x;
     };
+    
     $('.box-one').on('keyup', function(event) {
         var item = $('#submit-box1').val();
         console.log("1 item:", item);
@@ -26,16 +31,16 @@ $(document).ready(function () {
                 var rightArrow = $('<i class="fa fa-arrow-right"></i>');
                 //var xButton = newXbutton();
                 $('#items1 li:last').append(rightArrow, newXbutton());
-                newli(1);
+                newLi();
                 console.log('4 newli');
                 rightArrow.click(function() {
                     var content = $(this).parent().text();
                     console.log('8 content:', content);
-                    var move = $('#items2 li:last span').text(content);
+                    var move = $('#items2 dt:last span').text(content);
                     console.log('9 move right:', move);
-                    var addX = $('#items2 li:last').append(newXbutton());
+                    var addX = $('#items2 dt:last').append(newXbutton());
                     console.log('10 append x:', addX);
-                    newli(2);
+                    newDt();
                 })
                 $('#submit-box1').val('');
             } else {
@@ -46,40 +51,64 @@ $(document).ready(function () {
             $('#items1 li:last span').text(item);
         }
     })
+    
     $('#items1').on('click', 'li span', function() {
         $(this).toggleClass('crossout');
     })
-    $('.box-two').on('keyup', function(event) {
+    
+    $('#submit-box2').on('keyup', function(event) {
         var item = $('#submit-box2').val();
         console.log("1 item:", item);
         if (event.which === 13) {
             console.log("2 enter key");
             event.preventDefault();
-            if (item.length >= 1) {
-                console.log("3 true");
-                //var rightArrow = $('<i class="fa fa-arrow-right"></i>');
-                //var xButton = newXbutton();
-                $('#items2 li:last').append(newXbutton());
-                newli(2);
-                console.log('4 newli');
-                $('#submit-box2').val('');
-            } else {
+            if (item.length === 0) {
                 console.log("false");
                 alert("Please type in an item!");
+                return;
             }
+            $('#items2 dt:last').append(newXbutton());
+            newDt();
+            $('#submit-box2').val(''); 
         } else {
-            $('#items2 li:last span').text(item);
+            $('#items2 dt:last span').text(item);
+        }    
+    })
+    
+    $('#add-category').on('keyup', function(event) {
+        var categoryItem = $('#add-category').val();
+        if (event.which === 13) {
+            console.log("2 enter key");
+            event.preventDefault();
+            if (categoryItem.length === 0) {
+                console.log("false");
+                alert("Please type in a category!");
+                return;
+            }         
+            $('#items2 dt:last').append(newXbutton());
+            newDt();
+            $('#add-category').val('');
+        } else {
+            $('#items2 dt:last span').text(categoryItem);
         }
     })
-    var empty = function(n) {
-        var trash = $('#trash' + n);
-        console.log(trash);
-        trash.click(function() {
-            $('#items' + n).empty();
-            $('#items' + n).append('<ol></ol>');
-            newli(n);
+    
+    var emptyShoppingList = function() {
+        var trashShoppingList = $('#trash1');
+        console.log(trashShoppingList);
+        trashShoppingList.click(function() {
+            $('#items1 ol').empty();
+            newLi();
         });
     }
-    empty(1);
-    empty(2);
+    var emptyInventory = function() {
+        var trashInventory = $('#trash2');
+        console.log(trashInventory);
+        trashInventory.click(function() {
+            $('#items2 dl').empty();
+            newDt();
+        });
+    }
+    emptyShoppingList();
+    emptyInventory();
 });
