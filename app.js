6ddc5bc587f5c7b01a23b6969420d75a8941926c
Jearranai Jujaroen <jear.jujaroen@gmail.com>
2015-05-22 14:33:00 -0400

    Make #submit-box2 work, created an add category input, and fixed bug on initial page load using newli(2)

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ $(document).ready(function () {
         $('#items' + n + ' ol').append('<li><span></span></li>');
     };
     newli(1);
+    newli(2);
     var newXbutton = function() {
         var x = $('<i class="fa fa-times"></i>');
         x.click(function() {          
@@ -28,15 +29,15 @@ $(document).ready(function () {
                 newli(1);
                 console.log('4 newli');
                 rightArrow.click(function() {
-                    newli(2);
                     var content = $(this).parent().text();
                     console.log('8 content:', content);
                     var move = $('#items2 li:last span').text(content);
                     console.log('9 move right:', move);
-                    var debug = $('#items2 li:last').append(newXbutton());
-                    console.log('10 append x:', debug);
+                    var addX = $('#items2 li:last').append(newXbutton());
+                    console.log('10 append x:', addX);
+                    newli(2);
                 })
-                $('input').val('');
+                $('#submit-box1').val('');
             } else {
                 console.log("false");
                 alert("Please type in an item!");
@@ -48,7 +49,28 @@ $(document).ready(function () {
     $('#items1').on('click', 'li span', function() {
         $(this).toggleClass('crossout');
     })
-    
+    $('.box-two').on('keyup', function(event) {
+        var item = $('#submit-box2').val();
+        console.log("1 item:", item);
+        if (event.which === 13) {
+            console.log("2 enter key");
+            event.preventDefault();
+            if (item.length >= 1) {
+                console.log("3 true");
+                //var rightArrow = $('<i class="fa fa-arrow-right"></i>');
+                //var xButton = newXbutton();
+                $('#items2 li:last').append(newXbutton());
+                newli(2);
+                console.log('4 newli');
+                $('#submit-box2').val('');
+            } else {
+                console.log("false");
+                alert("Please type in an item!");
+            }
+        } else {
+            $('#items2 li:last span').text(item);
+        }
+    })
     var empty = function(n) {
         var trash = $('#trash' + n);
         console.log(trash);
